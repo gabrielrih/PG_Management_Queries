9d30cfe534acf6498aaf0125460f17de87cc1310
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2023-06-02 11:53:57 -0300

    fix: kill all process from db

--- kill_all_process_from_db.sql
+++ kill_all_process_from_db.sql
@@ -1,5 +1,4 @@
-SELECT 
-    pg_terminate_backend(pid) 
-FROM 
-    pg_stat_activity 
-WHERE datname = 'database_name'
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'TARGET_DB'
+  AND pid <> pg_backend_pid();
