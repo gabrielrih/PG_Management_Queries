37fbc95e610fa9fd0113a359f0e545b4e7e1a86d
Gabriel Richter <gabrielrih@gmail.com>
2025-02-04 11:07:44 -0300

    Update get-table-estimated-rows.sql

--- get-table-estimated-rows.sql
+++ get-table-estimated-rows.sql
@@ -1,4 +1,9 @@
-SELECT relname AS table_name,
-       reltuples::bigint AS estimated_rows
-FROM pg_class
-WHERE relname = 'table-name';
+select 
+	a.relname AS table_name,
+	b.nspname as schema_name,
+	a.reltuples::bigint AS estimated_rows
+FROM pg_class a
+inner join pg_namespace b on a.relnamespace = b.oid
+where
+	a.relname = 'table-name' and
+	b.nspname = 'schema-name';
